8748ce0d02022a06284b3064d5d14d0f25165d14
JMO <101443468+JoseManuelOberreuter@users.noreply.github.com>
2024-11-13 16:48:42 -0300

    Initial Commit

---
+++ JavaBasics/ArrayBucles.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class ArrayBucles {
+    public static void main(String[] args) {
+        String myString = "Esta es mi cadena de texto"; // Variable String
+        String mySecondString = "Esta es mi segunda cadena de texto"; // Variable String
+        List<String> myList = new ArrayList<>(); // Creando lista tipo Array especifica para Strings
+        myList.add(myString + ", " + mySecondString); // Añadiendo las variables String al Array
+        System.out.println(myList);
+
+        Map<String, String> myMap = new HashMap<>(); // Usar Diamond Operator <>
+        myMap.put("Key1", myString); // Añadiendo un valor a la llave
+        myMap.put("Key2", mySecondString);
+        System.out.println(myMap);
+
+        // Bucle finito
+        for (int index = 0; index < myList.size(); index++) {
+            System.err.println(myList.get(index));
+        }
+    }
+}
